60f51b00aa8fb9bc1bb101cbac0a97eccd2b5d71
KC-HaroldLee <kklastchnpd@naver.com>
2022-06-14 00:00:13 +0900

    2일차

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,18 +1,53 @@
+const messageList = document.querySelector("ul")
+const nickForm =  document.querySelector("#nick")
+const messageForm =  document.querySelector("#message")
 // const socket = new WebSocket(`http://localhost:3000}`);
 const socket = new WebSocket(`ws://${window.location.host}`);
 
-socket.addEventListener("open", () =>{
-    console.log("Connected to Server ⭕")
-});
+function makeMessage(type, payload){
+    const msg = {type, payload}; 
+    return JSON.stringify(msg);
+}
+
+function handleOpen() {
+    console.log("Connected to Server ⭕");
+  }
+
+socket.addEventListener("open", handleOpen)
 
 socket.addEventListener("message", (message) =>{
-    console.log("New message: >>>", message.data, "<<< from the Server")
+    // console.log("New message:", message.data)
+    const li = document.createElement("li");
+    li.innerText = message.data;
+    messageList.append(li);
 });
 
 socket.addEventListener("close", () => {
     console.log("Disconnected from Server~❌")
-})
+});
 
-setTimeout(() => {
-    socket.send("hello from the browser!");
-  }, 1000);
+function handleSummit(e){
+    e.preventDefault();
+    const input = messageForm.querySelector("input");
+    socket.send(makeMessage("new_message", input.value));
+    const li = document.createElement("li");
+    li.innerText = `You ${input.value}`;
+    messageList.append(li);
+    input.value = "";
+}
+
+function handleNickSummit(e){
+    e.preventDefault();
+    const input = nickForm.querySelector("input");
+    socket.send(
+        // { //이 형식은 유지되는가?
+        // type:"nickname",
+        // payload:input.value,
+        // }
+        makeMessage("nickname", input.value)
+    );
+    input.value = "";
+}
+
+messageForm.addEventListener("submit", handleSummit);
+nickForm.addEventListener("submit", handleNickSummit)
